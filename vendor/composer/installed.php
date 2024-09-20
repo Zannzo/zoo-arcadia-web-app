897fc44adce16ca45a3dda4387853128664819dd
Zannzo <152790874+Zannzo@users.noreply.github.com>
2024-09-20 21:24:26 +0200

    Ajout du fichier composer.lock et autres modifications

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1f26b8509789a8d4924b861e0eac7cc93c690dc9',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1f26b8509789a8d4924b861e0eac7cc93c690dc9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
